5e8228645e2f72d53b53a62f9ecc2e940de972c3
annissa <annissabal@gmail.com>
2025-07-02 10:01:53 +0800

    adding and viewing classes

---
+++ src/main/java/com/attendance/attendance/classManagement/dto/ClassRequest.java
@@ -0,0 +1,9 @@
+package com.attendance.attendance.classManagement.dto;
+
+public class ClassRequest {
+    private String className;
+
+    //getter and setter
+    public String getClassName() { return className;}
+    public void setClassName(String className) {this.className = className;}
+}
---
+++ src/main/java/com/attendance/attendance/classManagement/entity/ClassEntity.java
@@ -0,0 +1,5 @@
+package com.attendance.attendance.classManagement.entity;
+
+public class ClassEntity {
+    
+}
