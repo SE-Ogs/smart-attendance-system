c383066e1eaf5e8dcda1795339e94b95a4bf620b
toot-a-loot <mjartgithub@gmail.com>
2025-07-02 12:13:38 +0800

    class report api controller and service

--- src/main/java/com/attendance/attendance/ipAddress/AttendanceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.attendance.attendance.ipAddress;
-
-import com.attendance.attendance.ipAddress.Attendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-    int countBySessionId(Long sessionId);
-}
--- src/main/java/com/attendance/attendance/session/SessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.attendance.attendance.session;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SessionRepository extends JpaRepository<Session, Long> {
-    List<Session> findByTeacherId(Long teacherId);
-}
